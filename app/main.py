e00d943cbf4e763b49dce88289decd7472e12205
Ruslan M <19166472+remsed@users.noreply.github.com>
2024-04-05 21:54:41 +0200

    Add login screen via login.kv

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+#from kivy.core.window import Window
+#Window.size = (720, 1600)
 
 class MainApp(MDApp):
+
     def build(self):
-        self.theme_cls.primary_palette = "DeepPurple"
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        global screen_manager
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        return screen_manager
 
 if __name__ == '__main__':
     app = MainApp()
